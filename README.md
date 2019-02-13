cd90927ea1b70ef87f8d9844b01d3c14be065efd
Chris Marchesi <chrism@vancluevertech.com>
2019-02-12 19:05:47 -0800

    Initial release

---
+++ README.md
@@ -0,0 +1,35 @@
+# terraform-null-module
+
+Module `terraform-null-module` is a demonstration module,
+consisting of a single [`null_data_source`][null-data-source] data
+source, and a single [`null_resource`][null-resource] resource.
+
+This module ultimately does nothing, but can be used to do an
+end-to-end test of Terraform's registry functionality.
+
+[null-data-source]: https://www.terraform.io/docs/providers/null/data_source.html
+[null-resource]: https://www.terraform.io/docs/providers/null/resource.html
+
+Usage Example:
+
+    module "null_module" {
+      source  = "vancluever/terraform-null-module"
+      version = "1.0.0"
+      input   = "foo"
+      trigger = "one"
+    }
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| input | The input value for the `null_data_source` data source in this module. | string | `"foo"` | no |
+| trigger | The trigger value for the `null_resource` resource in this module. | string | `"one"` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| null\_data\_source\_id | The `id` of the `null_data_source` data source in this module. |
+| null\_resource\_id | The `id` of the `null_resource` resource in this module. |
+
---
+++ main.tf
@@ -0,0 +1,55 @@
+/**
+ * # terraform-null-module
+ * 
+ * Module `terraform-null-module` is a demonstration module,
+ * consisting of a single [`null_data_source`][null-data-source] data
+ * source, and a single [`null_resource`][null-resource] resource.
+ *
+ * This module ultimately does nothing, but can be used to do an
+ * end-to-end test of Terraform's registry functionality.
+ * 
+ * [null-data-source]: https://www.terraform.io/docs/providers/null/data_source.html
+ * [null-resource]: https://www.terraform.io/docs/providers/null/resource.html
+ * 
+ * Usage Example:
+ * 
+ *     module "null_module" {
+ *       source  = "vancluever/terraform-null-module"
+ *       version = "1.0.0"
+ *       input   = "foo"
+ *       trigger = "one"
+ *     }
+ * 
+ */
+
+variable "input" {
+  description = "The input value for the `null_data_source` data source in this module."
+  default     = "foo"
+}
+
+variable "trigger" {
+  description = "The trigger value for the `null_resource` resource in this module."
+  default     = "one"
+}
+
+data "null_data_source" "data" {
+  inputs = {
+    key = "${var.input}"
+  }
+}
+
+resource "null_resource" "resource" {
+  triggers = {
+    number = "${var.trigger}"
+  }
+}
+
+output "null_data_source_id" {
+  description = "The `id` of the `null_data_source` data source in this module."
+  value       = "${data.null_data_source.id}"
+}
+
+output "null_resource_id" {
+  description = "The `id` of the `null_resource` resource in this module."
+  value       = "${null_resource.id}"
+}
